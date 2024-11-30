a594c9750d1fdf0ccfed57afed337282137739f2
Jiyada22 <jiyadakreenawong@gmail.com>
2024-11-30 22:50:56 +0700

    code

---
+++ Lecture02-DataTypes/demovarletconst.js
@@ -0,0 +1,15 @@
+var hello = "Hello";
+
+console.log(hello);
+hello ="Hello World";
+
+if (true) {
+    let world = "Hello Wolrd";
+    console.log(hello);
+}
+
+//console.log(world);
+const fixval = "Fix Value";
+
+console.log(fixval);
+// fixva = "test assigne value to const"
